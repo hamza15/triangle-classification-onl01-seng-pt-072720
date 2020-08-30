f01ae057d0a99701c18f54515e9c2b97da50bd91
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-30 22:59:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,8 @@ class Triangle
       :equilateral
     elsif one == two || two == three || one == three
       :isosceles 
-    elsif one != two && two != three
+    else
+      :scalene
   end
   
 end
